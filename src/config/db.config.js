3ba900a1577bf19c23b461010fdc2179e5cba96f
AbilioGamaNJ <abiliogamanetojunior@gmail.com>
2023-11-27 23:00:07 -0300

    Mudança na senha:senhasql123

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "1234",
+    PASSWORD: "senhasql123",
     DB: "idosoSobcuidado_server_localhost",
     DIALECT : "postgres",
     pool:{
